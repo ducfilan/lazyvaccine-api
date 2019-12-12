37d3c106b07306a3e8d176d837b32a3576be7a5c
Duc Filan <ducfilan@gmail.com>
2019-12-12 18:20:34 +0900

    Api - Get all categories

--- src/routes/categories.route.js
+++ src/routes/categories.route.js
@@ -5,6 +5,6 @@ import CategoriesController from '../controllers/categories.controller'
 
 const router = new Router()
 
-router.route('/').get(CategoriesController.apiGetCategories)
+router.route('/:lang').get(CategoriesController.apiGetCategories)
 
 export default router
