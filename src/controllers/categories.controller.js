7120abedf36c3bf01036c445e324582fb2203cca
Duc Filan <ducfilan@gmail.com>
2020-02-13 17:46:48 +0900

    Fix get categories api empty

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -4,7 +4,8 @@ export default class CategoriesController {
   static async apiGetCategories(req, res, next) {
     try {
       let lang = req.params.lang
-      return res.json(CategoriesDao.getAllCategories(lang))
+
+      return res.json(await CategoriesDao.getAllCategories(lang))
     } catch (e) {
       console.log(`api, ${e}`)
       res.status(500).json({ error: e })
