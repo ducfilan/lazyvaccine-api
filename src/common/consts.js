165b19ff108f9dff48e589c7830017b3b56e5e3a
Duc Filan <ducfilan@gmail.com>
2021-09-21 00:14:49 +0800

    Add consts

--- src/common/consts.js
+++ src/common/consts.js
@@ -6,7 +6,15 @@ export default {
 export const SupportingLanguages = ['ar', 'zh', 'nl', 'en', 'de', 'it', 'ja', 'ko', 'mn', 'pt', 'ru', 'sl', 'es', 'vi']
 
 export const UserCollectionName = 'users'
+export const SetCollectionName = 'sets'
 
 export const LoginTypes = {
   google: 'google'
 }
+
+export const BaseCollectionProperties = {
+  last_updated: new Date(),
+  del_flag: false
+}
+
+export const SupportingSetTypes = ['term-def', 'question-answers', 'content']
