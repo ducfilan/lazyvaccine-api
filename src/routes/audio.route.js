033afbbb610eafa22fe47605e8ecb66d2c3c76b2
Duc Filan <ducfilan@gmail.com>
2022-08-08 15:47:36 +0800

    Remove multer for audio

--- src/routes/audio.route.js
+++ src/routes/audio.route.js
@@ -1,12 +1,9 @@
 import { Router } from 'express'
 import AudioController from '../controllers/audio.controller'
-import multer from 'multer'
 import auth from '../middlewares/global/auth.mw'
 
 const router = new Router()
 
-const upload = multer()
-
-router.route('/pronounce').get(auth, upload.none(), AudioController.apiGetPronounce)
+router.route('/pronounce').get(auth, AudioController.apiGetPronounce)
 
 export default router
